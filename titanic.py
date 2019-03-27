6c66eea28f27555a14c6b22b84d64d92326b3f80
Alessandro Pina <pina.alessandro17@gmail.com>
2019-03-27 19:26:21 +0100

    DAY 2: Data cleaning and analysis

--- titanic.py
+++ titanic.py
@@ -4,26 +4,60 @@ import sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.preprocessing import LabelEncoder
 
 # Load the train and test data
 train = pd.read_csv("data/train.csv")
 test = pd.read_csv("data/test.csv")
-full_data = [train, test]
 
-print("Null values in training:\n", train.isnull().sum())
-print("Null values in test:\n", test.isnull().sum())
+# Histograms
+g = sns.FacetGrid(train, col='Survived')
+g.map(plt.hist, 'Age', bins=20)
+plt.show()
 
 # Drop unnecessary features
-train = train.drop(["Name", "Ticket"], axis=1)
-test = test.drop(["Name", "Ticket"], axis=1)
+drop_features = ['PassengerId', 'Cabin', 'Name', 'Ticket']
+train = train.drop(drop_features, axis=1)
+test = test.drop(drop_features, axis=1)
+full_data = [train, test]
 
-# Create new feature FamilySize as a combination of SibSp and Parch
+
+# Analysis of possible correlations
+for x in train:
+    if train[x].dtype != 'float64' and x != 'Survived':
+        print('Survival Correlation by:', x)
+        print(train[[x, 'Survived']].groupby(x, as_index=False).
+              mean().sort_values(by='Survived', ascending=False))
+        print('-'*10, '\n')
+
+
+# Completing data
+for dataset in full_data:
+    dataset['Age'].fillna(dataset['Age'].median(), inplace=True)
+    dataset['Embarked'].fillna(dataset['Embarked'].mode()[0], inplace=True)
+    dataset['Fare'].fillna(dataset['Fare'].median(), inplace=True)
+
+# Creation of new features
 for dataset in full_data:
     dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
-# Create new feature IsAlone from FamilySize
+
 for dataset in full_data:
     dataset['IsAlone'] = 0
     dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
-train["Embarked"] = train["Embarked"].fillna("S")
 
+# Continuous variable bins
+for dataset in full_data:
+    dataset['FareBin'] = pd.qcut(dataset['Fare'], 4)
+    dataset['AgeBin'] = pd.cut(dataset['Age'].astype(int), 5)
 
+# Convert objects to categories
+for dataset in full_data:
+    dataset['Sex_Code'] = LabelEncoder().fit_transform(dataset['Sex'])
+    dataset['Embarked_Code'] = LabelEncoder().fit_transform(dataset['Embarked'])
+    dataset['AgeBin_Code'] = LabelEncoder().fit_transform(dataset['AgeBin'])
+    dataset['FareBin_Code'] = LabelEncoder().fit_transform(dataset['FareBin'])
+
+# Drop unnecessary features
+drop_features = ['Age', 'AgeBin', 'Embarked', 'FamilySize', 'Fare', 'FareBin',
+                 'Parch', 'Sex', 'SibSp']
+train = train.drop(drop_features, axis=1)
